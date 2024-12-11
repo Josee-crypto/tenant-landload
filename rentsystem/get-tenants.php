780329b4dfd9dc0c9d52ad2bec52dbf8fd2149e0
Josee-crypto <arakajoseph8343@gmail.com>
2024-12-11 12:46:16 +0300

    Delete rentsystem directory

--- rentsystem/get-tenants.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('config.php');
-$stmt = $pdo->prepare("SELECT * FROM users WHERE user_type = 'tenant'");
-$stmt->execute();
-$tenants = $stmt->fetchAll();
-echo json_encode($tenants);
-?>
